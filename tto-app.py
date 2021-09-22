43d20b3a5fa51d5c99e6ca58d6276fd36aa7b73c
Manuel Castiblanco <71740021+mcastiblanco1251@users.noreply.github.com>
2021-09-22 13:53:12 -0500

    Add files via upload

--- tto-app.py
+++ tto-app.py
@@ -18,7 +18,7 @@ import smtplib
 im = Image.open("rutas.jpg")
 
 st.set_page_config(page_title='Optimizacion', layout="wide", page_icon=im)
-#st.set_option('deprecation.showPyplotGlobalUse', False)
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # LAYING OUT THE TOP SECTION OF THE APP
 
