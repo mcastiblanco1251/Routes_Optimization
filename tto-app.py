f1f35d71716dfe9faecc29d6f3c771d0cfdd2203
Manuel Castiblanco <71740021+mcastiblanco1251@users.noreply.github.com>
2024-12-16 11:28:50 -0500

    Add files via upload

--- tto-app.py
+++ tto-app.py
@@ -233,12 +233,12 @@ def plot_guess(city_coordinates, guess, guess_in_title=False):
         x = [city_coordinates[guess[ix]][0],city_coordinates[guess[ix+1]][0]]
         y = [city_coordinates[guess[ix]][1],city_coordinates[guess[ix+1]][1]]
         plt.plot(x,y,'c--',lw=1)
-    fig2=plt.scatter(city_coordinates[guess[0]][0],city_coordinates[guess[0]][1], marker='x', c='b')
+        plt.scatter(city_coordinates[guess[0]][0],city_coordinates[guess[0]][1], marker='x', c='b')
     if guess_in_title:
         plt.title("Current Guess: [%s]"%(','.join([str(x) for x in guess])))
     else:
         print("Current Guess: [%s]"%(','.join([str(x) for x in guess])))
-    return(fig,fig2)
+    return(fig)
 
 path = create_guess(list(test_locations.keys()))
 #st.write(path)#print(path)
