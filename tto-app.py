76f8ed5b1e7e35779664122113be73597a35b986
Manuel Castiblanco <71740021+mcastiblanco1251@users.noreply.github.com>
2024-12-16 11:26:47 -0500

    Add files via upload

--- tto-app.py
+++ tto-app.py
@@ -228,11 +228,11 @@ def plot_guess(city_coordinates, guess, guess_in_title=False):
     guess: list of ids in order
     """
     #plot_cities(city_coordinates)
-    fig=plt.figure()#dpi=250)
+    fig, ax=plt.figure()#dpi=250)
     for ix, current_city in enumerate(guess[:-1]):
         x = [city_coordinates[guess[ix]][0],city_coordinates[guess[ix+1]][0]]
         y = [city_coordinates[guess[ix]][1],city_coordinates[guess[ix+1]][1]]
-        plt.scatter(x,y,'c--',lw=1)
+        ax=plt.plot(x,y,'c--',lw=1)
     plt.scatter(city_coordinates[guess[0]][0],city_coordinates[guess[0]][1], marker='x', c='b')
     if guess_in_title:
         plt.title("Current Guess: [%s]"%(','.join([str(x) for x in guess])))
